eac4fda4ab1e9d22011ba0a6ba67377a679484a4
karthik <karthithelearner@gmail.com>
2023-09-05 10:02:39 +0530

    initial

---
+++ src/components/Box.jsx
@@ -0,0 +1,78 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+
+function Box({ numbers, onSelect,index }) {
+  const ballSize = 40; // Size of each ball in pixels
+  const boxWidth = 300; // Width of the container in pixels
+  const boxHeight = 300; // Height of the container in pixels
+  const numBalls = numbers.length;
+
+  // State to store the positions of the balls
+  const [ballPositions, setBallPositions] = useState([]);
+
+  // Function to check if a ball overlaps with another ball
+  const isOverlap = (x1, y1, r1, x2, y2, r2) => {
+    const dx = x1 - x2;
+    const dy = y1 - y2;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    return distance < r1 + r2;
+  };
+
+  // Function to generate random non-overlapping positions for the balls
+  const generateRandomPositions = () => {
+    const newBallPositions = [];
+    for (let i = 0; i < numBalls; i++) {
+      let left, top;
+      let isOverlapping = true;
+      while (isOverlapping) {
+        left = Math.random() * (boxWidth - ballSize);
+        top = Math.random() * (boxHeight - ballSize);
+        isOverlapping = false;
+
+        // Check for overlap with other balls
+        for (let j = 0; j < i; j++) {
+          const otherPosition = newBallPositions[j];
+          if (isOverlap(left, top, ballSize / 2, otherPosition.left, otherPosition.top, ballSize / 2)) {
+            isOverlapping = true;
+            break;
+          }
+        }
+
+        // Check for overlap with the box border
+        if (
+          left < ballSize / 2 || left + ballSize > boxWidth - ballSize / 2 ||
+          top < ballSize / 2 || top + ballSize > boxHeight - ballSize / 2
+        ) {
+          isOverlapping = true;
+        }
+      }
+      newBallPositions.push({ left, top });
+    }
+    setBallPositions(newBallPositions);
+  };
+
+  // Generate random positions when the component mounts
+  useEffect(() => {
+    generateRandomPositions();
+  }, []);
+
+  return (
+    <div
+      className='border border-violet-400 p-5 hover:shadow-2xl hover:shadow-violet-400 hover:cursor-pointer relative'
+      style={{ width: `${boxWidth}px`, height: `${boxHeight}px` }}
+      onClick={()=>onSelect(index)}
+    >
+      {ballPositions.map((position, index) => (
+        <div
+          key={numbers[index]}
+          className='m-3 flex justify-center items-center rounded-full border border-teal-300 absolute'
+          style={{ left: `${position.left}px`, top: `${position.top}px`, width: `${ballSize}px`, height: `${ballSize}px` }}
+        >
+          {numbers[index]}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Box;
---
+++ src/components/Boxes.jsx
@@ -0,0 +1,98 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Box from "./Box";
+
+function initialThreeSets(numbers){
+    const set1 = numbers.slice(0, 7);
+    const set2 = numbers.slice(7, 14);
+    const set3 = numbers.slice(14, 21);
+  return [set1,set2,set3]
+}
+
+function createThreeSets(numbers) {
+  const sets = [[], [], []];
+
+  for (let i = 0; i < numbers.length; i++) {
+    const setIndex = i % 3;
+    sets[setIndex].push(numbers[i]);
+  }
+
+  return sets;
+}
+
+function rearrangeSets(order, numbersSet) {
+    const [set1, set2, set3] = numbersSet;
+    let sets = [];
+  
+    if (order === 0) {
+      sets = [set3, set1, set2];
+    } else if (order === 1) {
+      sets = [set3, set2, set1];
+    } else if (order === 2) {
+      sets = [set2, set3, set1];
+    }
+  
+    // Combine all sets into a single set
+    const mergedSet = [].concat(...sets);
+  console.log(mergedSet)
+    return mergedSet;
+  }
+  
+  
+  
+
+function Boxes({ random }) {
+  //const [randomNumbers, setRandomNumbers] = useState(random);
+  //console.log(randomNumbers)
+  const [numbersSet, setNumbersSet] = useState(initialThreeSets(random));
+  console.log(numbersSet)
+const[tries, setTries]=useState(0);
+const[finalNumber,setFinalNumber]=useState("");
+
+  useEffect(() => {
+if (tries==3){
+        //const middleIndex = numbersSet[1][4];
+//const middleNumber = randomNumbers[middleIndex];
+setFinalNumber(numbersSet[1][3]);
+setTries(0)
+    }
+    
+    
+  }, [numbersSet, tries]);
+
+  const onSelect=(id)=>{
+
+    console.log(id)
+    const newNumbers=rearrangeSets(id,numbersSet);
+    //setRandomNumbers(newNumbers);
+    setNumbersSet(createThreeSets(newNumbers))
+    setTries(tries+1)
+    }
+  
+  return (
+    <div>
+        <div className="flex justify-center items-center">Number of Tries:{tries}</div>
+       { finalNumber!="" && <div className="fixed inset-0 z-50  flex justify-center items-center backdrop-blur-md">
+  <div className="w-[300px] h-[200px] border border-violet-400 flex flex-col p-2 ">
+    <div className="flex flex-row-reverse cursor-pointer" onClick={()=>setFinalNumber("")}>X</div>
+    <div className="flex-1 flex flex-col justify-center items-center">
+    <div>Your Number</div>
+    <div
+          className='w-[50px] h-[50px] m-3 shadow-2xl shadow-teal-400 flex justify-center items-center rounded-full border border-teal-300'
+        >
+          {finalNumber}
+        </div>
+        </div></div>
+</div> } 
+
+        {numbersSet.length>0 && <div className="flex flex-col gap-5 md:flex-row p-4">
+      <Box numbers={numbersSet[0]} onSelect={onSelect} index={0} />
+      <Box numbers={numbersSet[1]} onSelect={onSelect} index={1}/>
+      <Box numbers={numbersSet[2]} onSelect={onSelect} index={2}/>
+    </div> }
+    
+    </div>
+  );
+}
+
+export default Boxes;
